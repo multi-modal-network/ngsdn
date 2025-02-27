d68ebab48b9bae00652176956f991a9a0a998baf
Archor001 <m202271789@hust.edu.cn>
2025-02-27 20:01:32 +0800

    feat: 卫星模态拓扑

--- mininet/satellite_topo.py
+++ mininet/satellite_topo.py
@@ -28,18 +28,55 @@ from mininet.topo import Topo
 from mininet.node import Controller, RemoteController
 from mininet.link import Intf
 
+
+
+
 import subprocess
 
 CPU_PORT = 255
+
+def ovs_service_start():
+    try:
+        subprocess.check_call(["service", "openvswitch-switch", "start"])
+        print("Command executed successfully")
+    except subprocess.CalledProcessError as e:
+        print("An error occurred while trying to execute the command: {}".format(e))
+    except Exception as e:
+        print("An unexpected error occurred: {}".format(e))
+def addOVSPort(bridge, port):
+    try:
+        subprocess.check_call(["ovs-vsctl", "add-port", bridge, port])
+        print("Port added successfully")
+    except subprocess.CalledProcessError as e:
+        print("An error occurred while trying to execute the command: {}".format(e))
+    except Exception as e:
+        print("An unexpected error occurred: {}".format(e))
+def applyOVSFlow(bridge, flow):
+    try:
+        subprocess.check_call(["ovs-ofctl", "add-flow", bridge] + flow.split(","))
+        print("Flow applied sucessfully")
+    except subprocess.CalledProcessError as e:
+        print("An error occurred while trying to execute the command: {}".format(e))
+    except Exception as e:
+        print("An unexpected error occurred: {}".format(e))
+
 
 class TutorialTopo(Topo):
     """2x2 fabric topology with IPv6 hosts"""
 
     def __init__(self):
         Topo.__init__(self)
-        self.addSwitch("satellite1", cls=StratumBmv2Switch, cpuport=CPU_PORT)
-        self.addSwitch("satellite1", cls=StratumBmv2Switch, cpuport=CPU_PORT)
-        self.addSwitch("satellite1", cls=StratumBmv2Switch, cpuport=CPU_PORT)
+        ovs_service_start()
+        switch_list = []
+        s1 = self.addSwitch('s1', cls=StratumBmv2Switch, cpuport=CPU_PORT)
+        switch_list.append('s1')
+        ovs1 = self.addSwitch('ovs1')
+        self.addLink(ovs1, s1)
+        for i in range(2, 4):
+            switch_name = 's{}'.format(i)
+            switch = self.addSwitch(switch_name, cls=StratumBmv2Switch, cpuport=CPU_PORT)
+            switch_list.append(switch_name)
+
 
 
 switches = {'stratum-bmv2': StratumBmv2Switch}
@@ -50,8 +87,15 @@ topos = {'custom': (lambda: TutorialTopo())}
 
 
 def main():
+    #modify_port('/home/eis/P4/onos/tools/dev/mininet/stratum.py', 'nextGrpcPort', 60000)
+    #reload(stratum)
+    ovs_service_start()
     net = Mininet(topo=TutorialTopo(), controller=None)
+    #c0 = net.addController(name='c0', controller=RemoteController, ip='192.168.2.139', port=6653)
     net.start()
+
+    # 添加ovs
+
 
     CLI(net)
     net.stop()
